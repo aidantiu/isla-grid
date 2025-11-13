81730c98d8c5f2f89693a66b3b6baf6dec5f788b
Erwin <111296942+SauceCode01@users.noreply.github.com>
2025-11-14 02:39:18 +0800

    Feat/chat history api (#41)
    * feat(server): implemented chat history types
    
    * feat(server): implement chat api endpoints
    
    * feat(server): tested the api on postman
    
    * feat(client): implemented chat history api endpoint on frontend
    
    * feat(server): fixed the implementation of the userContextApi
    
    * feat(client): fixed frontend endpont
    
    * feat(server, client): endpoints done

---
+++ client/lib/apiEndpoints/chatEndpoints.ts
@@ -0,0 +1,102 @@
+import { ApiRequest, ApiResponse } from "@/types/apiTypes";
+import { Chat, Message } from "@/types/chatTypes";
+
+/**
+ * creates a chat for the authenticated user
+ */
+export const initializeChat = async (authToken: string): Promise<Chat> => {
+  const result = await fetch("http://localhost:8000/api/chats", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      authToken: authToken,
+    },
+  });
+
+  if (!result.ok) {
+    const resultPayload = (await result.json()) as ApiResponse;
+    throw new Error(resultPayload.message);
+  }
+
+  const resultPayload = (await result.json()) as ApiResponse<Chat>;
+
+  const newContext = resultPayload.data as Chat;
+
+  return newContext;
+};
+
+export const listAllChatsOfUser = async (
+  authToken: string
+): Promise<Chat[]> => {
+  const result = await fetch("http://localhost:8000/api/chats", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authToken: authToken,
+    },
+  });
+
+  if (!result.ok) {
+    const resultPayload = (await result.json()) as ApiResponse;
+    throw new Error(resultPayload.message);
+  }
+
+  const resultPayload = (await result.json()) as ApiResponse<Chat[]>;
+
+  const newContext = resultPayload.data as Chat[];
+
+  return newContext;
+};
+
+export const getSpecificChat = async (
+  authToken: string,
+  chatId: string
+): Promise<Chat> => {
+  const result = await fetch(`http://localhost:8000/api/chats/${chatId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authToken: authToken,
+    },
+  });
+
+  if (!result.ok) {
+    const resultPayload = (await result.json()) as ApiResponse;
+    throw new Error(resultPayload.message);
+  }
+
+  const resultPayload = (await result.json()) as ApiResponse<Chat>;
+
+  const newContext = resultPayload.data as Chat;
+
+  return newContext;
+};
+
+export const pushMessageToChat = async (
+  authToken: string,
+  chatId: string,
+  message: Message
+): Promise<Chat> => {
+  const requestBody: ApiRequest<Message> = {
+    payload: message,
+  };
+  const result = await fetch(`http://localhost:8000/api/chats/${chatId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authToken: authToken,
+    },
+    body: JSON.stringify(requestBody),
+  });
+
+  if (!result.ok) {
+    const resultPayload = (await result.json()) as ApiResponse;
+    throw new Error(resultPayload.message);
+  }
+
+  const resultPayload = (await result.json()) as ApiResponse<Chat>;
+
+  const newContext = resultPayload.data as Chat;
+
+  return newContext;
+};
