6781b0489c5938bb31732ef8e3d3c45f087f72db
SauceCode01 <sauce.code.01@gmail.com>
2025-11-13 11:05:37 +0800

    feat(server): implemented POST api/readings/

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { readingsRouter } from './routes/readingsRoute.js';
 
 
 const app = express();
@@ -9,10 +10,16 @@ const port = 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
+
+
 // Basic route for the homepage
 app.get('/', (req, res) => {
   res.send('Welcome to the Express.js Application!');
 });
+
+app.use("/api/readings", readingsRouter);
  
 // Start the server
 app.listen(port, () => {
