e2dc69c8cb9dce844c1b1313403e2881c506edf2
SauceCode01 <sauce.code.01@gmail.com>
2025-11-13 15:27:57 +0800

    feat(client): created api endpoint functions on the frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import { readingsRouter } from './routes/readingsRoute.js';
 import { contextRouter } from './routes/contextRoutes.js';
+import cors from "cors";
 
 
 const app = express();
 const port = 8000;
  
+// CORS config
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 
 // Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
